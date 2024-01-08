56891a23e16cc718a9e1ad29853181bcbd2c6a3a
KwangJoo Park <kjpar0317@innogrid.com>
2024-01-08 17:55:14 +0900

    보완

--- frontend/src/services/layout/useLayout.ts
+++ frontend/src/services/layout/useLayout.ts
@@ -2,24 +2,23 @@
 import { useCallback } from 'react';
 import useSWR from 'swr';
 
-type TEasePower1 = "power1.out" | "power1.inOut" | "power1.out";
-type TEasePower2 = "power2.out" | "power2.inOut" | "power2.out";
-type TEasePower3 = "power3.out" | "power3.inOut" | "power3.out";
-type TEasePower4 = "power4.out" | "power4.inOut" | "power4.out";
-type TEaseBack = "back.out" | "back.inOut" | "back.out";
-type TEaseBounce = "bounce.out" | "bounce.inOut" | "bounce.out";
-type TEaseCirc = "circ.out" | "circ.inOut" | "circ.out";
-type TEaseElastic = "elastic.out" | "elastic.inOut" | "elastic.out";
-type TEaseExpo = "expo.out" | "expo.inOut" | "expo.out";
-type TEaseSinc = "sinc.out" | "sinc.inOut" | "sinc.out";
-type TActionType = "fade" | "transform" | "shuffle";
+type TEasePower1 = "power1.out" | "power1.inOut" | "power1.in";
+type TEasePower2 = "power2.out" | "power2.inOut" | "power2.in";
+type TEasePower3 = "power3.out" | "power3.inOut" | "power3.in";
+type TEasePower4 = "power4.out" | "power4.inOut" | "power4.in";
+type TEaseBack = "back.out" | "back.inOut" | "back.in";
+type TEaseBounce = "bounce.out" | "bounce.inOut" | "bounce.in";
+type TEaseCirc = "circ.out" | "circ.inOut" | "circ.in";
+type TEaseElastic = "elastic.out" | "elastic.inOut" | "elastic.in";
+type TEaseExpo = "expo.out" | "expo.inOut" | "expo.in";
+type TEaseSinc = "sinc.out" | "sinc.inOut" | "sinc.in";
 
 export type TEaseType = TEasePower1 | TEasePower2 | TEasePower3 | TEasePower4 | TEaseBack | TEaseBounce | TEaseCirc | TEaseElastic | TEaseExpo | TEaseSinc | "steps";
-export type TDivAction = TActionType;
-export type TInputAction = TActionType;
+export type TDivAction = "fade" | "transform" | "shuffle";
+export type TInputAction = "fade" | "transform" | "shuffle";
 
 export interface ILayoutStore {
-    theme?: string | undefined;
+    theme?: string;
     setTheme?: (val: string) => void;
 } 
 
