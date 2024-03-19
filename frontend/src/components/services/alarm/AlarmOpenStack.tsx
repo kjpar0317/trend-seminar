eac5d1fc7128b7619b2099a2129d9a1eca1c1da3
kjpar0317 <kjpar0317@gmail.com>
2024-03-19 23:57:22 +0900

    테스트

--- frontend/src/components/services/alarm/AlarmOpenStack.tsx
+++ frontend/src/components/services/alarm/AlarmOpenStack.tsx
@@ -1,14 +1,21 @@
-import { ReactElement, useEffect } from "react";
-
+import { ReactElement, useState, useEffect } from "react";
 import { gsap } from "gsap";
-import { random } from "lodash-es";
+import { range, random } from "lodash-es";
 
 export default function AlarmOpenStack(): ReactElement {
-  function handleGo() {
-    const randDur: number = random(1.2, 2.0);
-    const randStag: number = random(0.5, 1.2);
+  const [dup, setDup] = useState<number>(1);
 
-    gsap.from(".acolumns", {
+  useEffect(() => {
+    handleAnimate();
+  }, [dup]);
+
+  function handleAnimate() {
+    const randDur: number = random(1.5, 2.5);
+    const randStag: number = random(1, 5);
+    const bounceTimeline = gsap.timeline({});
+
+    bounceTimeline.clear();
+    bounceTimeline.from(".acolumns", {
       x: -1500,
       duration: randDur,
       autoAlpha: 1,
@@ -18,31 +25,47 @@ export default function AlarmOpenStack(): ReactElement {
       },
       repeat: -1,
     });
+    // bounceTimeline.to(".acolumns", { delay: 2 }, 1);
+  }
+
+  function handleIncrement() {
+    setDup(dup + 1);
+  }
+
+  function handleDecrement() {
+    setDup(dup - 1);
   }
 
   return (
     <>
       <div className={"flex"}>
-        <button className={"btn btn-primary"} onClick={handleGo}>
-          GO~GO~GO~
+        <button className={"btn btn-primary"} onClick={handleIncrement}>
+          increase
+        </button>
+        <button className={"btn btn-primary"} onClick={handleDecrement}>
+          decrease
         </button>
       </div>
       <div id={"test"} className={"relative flex grid-cols-1 float-right"}>
-        <div className={"absolute top-1 acolumns bg-amber-100 right-20"}>
-          adfdafd1
-        </div>
-        <div className={"absolute top-4 acolumns bg-amber-200 right-20"}>
-          adfdafd2
-        </div>
-        <div className={"absolute top-8 acolumns bg-amber-300 right-20"}>
-          adfdafd3
-        </div>
-        <div className={"absolute top-12 acolumns bg-amber-400 right-20"}>
-          adfdafd4
-        </div>
-        <div className={"absolute top-16 acolumns bg-amber-500 right-20"}>
-          adfdafd5
-        </div>
+        {range(0, dup, 1).map((index) => (
+          <>
+            <div className={"absolute top-1 acolumns bg-amber-100 right-20"}>
+              adfdafd1
+            </div>
+            <div className={"absolute top-4 acolumns bg-amber-200 right-20"}>
+              adfdafd2
+            </div>
+            <div className={"absolute top-8 acolumns bg-amber-300 right-20"}>
+              adfdafd3
+            </div>
+            <div className={"absolute top-12 acolumns bg-amber-400 right-20"}>
+              adfdafd4
+            </div>
+            <div className={"absolute top-16 acolumns bg-amber-500 right-20"}>
+              adfdafd5
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
