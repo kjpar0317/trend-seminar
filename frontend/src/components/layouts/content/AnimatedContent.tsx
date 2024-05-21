367824336ee94766f925310df8147098dd39f26c
박광주 <kjpar0317@innogrid.com>
2024-05-21 16:55:09 +0900

    test 추가

---
+++ frontend/src/components/layouts/content/AnimatedContent.tsx
@@ -0,0 +1,45 @@
+"use client";
+import type { ReactNode } from "react";
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import usePrefersReducedMotion from "@/hook/usePrefersReducedMotion";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function AnimatedContent({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  const container = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  useGSAP(
+    () => {
+      if (prefersReducedMotion) {
+        gsap.set(container.current, { y: 0 });
+        return;
+      }
+
+      gsap.fromTo(
+        container.current,
+        { y: 100 },
+        {
+          y: 0,
+          ease: "power2.inOut",
+          duration: 1,
+          scrollTrigger: {
+            trigger: container.current,
+            start: "top bottom-=40%",
+            toggleActions: "play pause resume reverse",
+          },
+        }
+      );
+    },
+    { scope: container }
+  );
+
+  return <div ref={container}>{children}</div>;
+}
